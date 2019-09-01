93c1184dfb0a5044d048033f2aba1c8aea6e335e
Kris Torres <kristorresdayo@gmail.com>
2019-08-31 22:35:25 -0700

    Implement the custom useComponentDidMount hook

---
+++ src/hooks.js
@@ -0,0 +1,14 @@
+import {useEffect} from "react";
+
+const useComponentDidMount = (handler) => {
+    useEffect(
+        () => {
+            handler()
+        },
+        []
+    );
+};
+
+export {
+    useComponentDidMount
+};
