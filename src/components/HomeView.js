0b05b35712d98dc62c10e149a359db345956ea39
Kris Torres <kristorresdayo@gmail.com>
2019-08-31 18:53:49 -0700

    Create a blank “home view”

---
+++ src/components/HomeView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import FlexBox from "./FlexBox.js";
+
+function HomeView() {
+    return (
+        <FlexBox component="main" level={1} className="mdc-theme--background"/>
+    );
+}
+
+export default HomeView;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,14 +2,14 @@ import React from "react";
 
 import FlexBox from "./FlexBox.js";
 import Footer from "./Footer.js";
-import HelloWorld from "./HelloWorld.js";
+import HomeView from "./HomeView.js";
 import NavigationBar from "./NavigationBar.js";
 
 function Layout() {
     return (
         <FlexBox style={{minHeight: "100vh"}}>
             <NavigationBar/>
-            <HelloWorld/>
+            <HomeView/>
             <Footer/>
         </FlexBox>
     );
