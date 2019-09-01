105d855e36d861bfb4beb6c81cac5ec80f726454
Kris Torres <kristorresdayo@gmail.com>
2019-09-01 11:17:49 -0700

    Append a missing semicolon to the handler call

--- src/hooks.js
+++ src/hooks.js
@@ -3,7 +3,7 @@ import {useEffect} from "react";
 const useComponentDidMount = (handler) => {
     useEffect(
         () => {
-            handler()
+            handler();
         },
         []
     );
