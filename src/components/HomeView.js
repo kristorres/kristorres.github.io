9c77cdfb3dc1b318a5419ac7f83cde5829caf639
Kris Torres <kristorresdayo@gmail.com>
2019-08-31 21:29:57 -0700

    Add a blank header to the home view

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -2,9 +2,30 @@ import React from "react";
 
 import FlexBox from "./FlexBox.js";
 
+const styles = {
+    header: {
+        backgroundColor: "#37474F",
+        backgroundImage: "radial-gradient(#37474F 25%, #607D8B)"
+    }
+};
+
+function Header() {
+    return (
+        <FlexBox
+            component="header"
+            level={1}
+            justifyContent="center"
+            alignItems="center"
+            style={styles.header}
+        />
+    );
+}
+
 function HomeView() {
     return (
-        <FlexBox component="main" level={1} className="mdc-theme--background"/>
+        <FlexBox component="main" level={1} className="mdc-theme--background">
+            <Header/>
+        </FlexBox>
     );
 }
 
