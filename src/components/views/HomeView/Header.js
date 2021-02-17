c2e0ea903061efb39e63ee31c0e5517254271410
Kris Torres <kristorresdayo@gmail.com>
2021-02-17 10:16:16 -0800

    Rewrite Header with styled-components

--- src/components/views/HomeView/Header.js
+++ src/components/views/HomeView/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-import FlexBox from "../../FlexBox.js";
-import {useComponentDidMount, useMedia} from "../../../hooks.js";
+import {useComponentDidMount} from "../../../hooks.js";
 
 const Domains = Object.freeze({
     Images: "<PUBLIC DIRECTORY>images"
@@ -13,41 +12,60 @@ const ImageURLs = Object.freeze({
 });
 
 const Colors = Object.freeze({
+    Background: "#0C3E36",
     Border: "rgba(255, 255, 255, 0.5)",
     Text: "white"
 });
 
-function Avatar() {
-    const imageWidth = 250;
-    const borderWidth = 8;
-    const style = {
-        border: `${borderWidth}px solid ${Colors.Border}`,
-        borderRadius: (imageWidth + borderWidth * 2) / 2,
-        width: imageWidth,
-        height: imageWidth
-    };
-    return <img style={style} src={ImageURLs.Avatar}/>;
-}
+const Wrapper = styled.header`
+    background-color: ${Colors.Background};
+    background-image: url("${ImageURLs.Header}");
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+    padding: 48px 24px;
+`;
 
-function TypingCarousel() {
-    const windowWidthIsAtLeastSmall = useMedia("(min-width: 600px)");
-    const style = {
-        backgroundColor: "rgba(0, 0, 0, 0.25)",
-        border: `8px solid ${Colors.Border}`,
-        borderRadius: 16,
-        fontFamily: "Menlo, Monaco, Consolas, 'Courier New', monospace",
-        fontSize: 32,
-        color: Colors.Text,
-        whiteSpace: "pre",
-        overflow: "scroll",
-        WebkitUserSelect: "none",
-        MozUserSelect: "none",
-        MsUserSelect: "none",
-        userSelect: "none",
-        width: windowWidthIsAtLeastSmall ? 512 : "calc(100% - 48px)",
-        height: 48,
-        padding: 12
-    };
+const Avatar = styled.img`
+    display: block;
+    border: 8px solid ${Colors.Border};
+    border-radius: 9999px;
+    width: 250px;
+    box-shadow: 0 0 1px 8px rgba(255, 255, 255, 0.25), 0 0 1px 16px rgba(255, 255, 255, 0.125);
+    margin: 0 auto;
+`;
+
+const Title = styled.h1`
+    font-family: Oswald, HelveticaNeue-CondensedBold, sans-serif;
+    font-size: 96px;
+    font-weight: 700;
+    letter-spacing: -1.5px;
+    text-align: center;
+    text-transform: uppercase;
+    color: ${Colors.Text};
+    &::selection {
+        background-color: ${Colors.Text};
+        color: ${Colors.Background};
+    }
+`;
+
+const TypingCarousel = styled.div`
+    background-color: rgba(0, 0, 0, 0.25);
+    border: 8px solid ${Colors.Border};
+    border-radius: 16px;
+    font-family: SFMono-Regular, Menlo, ui-monospace, Monaco, Consolas, "Courier New", monospace;
+    font-size: 32px;
+    color: ${Colors.Text};
+    white-space: pre;
+    overflow: scroll;
+    user-select: none;
+    max-width: 512px;
+    height: 1.125em;
+    padding: 0.375em;
+    margin: 0 auto;
+`;
+
+function Header() {
     useComponentDidMount(
         () => {
             new Typed(
@@ -68,43 +86,11 @@ function TypingCarousel() {
         }
     );
     return (
-        <FlexBox
-            className="typing-carousel"
-            justifyContent="center"
-            style={style}
-        />
-    );
-}
-
-function Header() {
-    const styles = {
-        root: {
-            backgroundColor: "#0C3E36",
-            backgroundImage: `url("${ImageURLs.Header}")`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            padding: "48px 24px"
-        },
-        title: {
-            textAlign: "center",
-            textTransform: "uppercase",
-            color: Colors.Text
-        }
-    };
-    return (
-        <FlexBox
-            component="header"
-            level={1}
-            justifyContent="center"
-            alignItems="center"
-            style={styles.root}
-        >
-            <Avatar/>
-            <h1 className="mdc-typography--headline1" style={styles.title}>
-                Kris Torres
-            </h1>
-            <TypingCarousel/>
-        </FlexBox>
+        <Wrapper>
+            <Avatar src={ImageURLs.Avatar}/>
+            <Title>Kris Torres</Title>
+            <TypingCarousel className="typing-carousel"/>
+        </Wrapper>
     );
 }
 
