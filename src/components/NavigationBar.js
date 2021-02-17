82d73d0bd3d6a5c549acc221731d6f27d863a8e9
Kris Torres <kristorresdayo@gmail.com>
2021-02-16 17:03:49 -0800

    Rewrite NavigationBar with styled-components

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,23 +1,28 @@
 import React from "react";
 
-const itemAlignmentClassName = (value) => {
-    const sectionClassName = "mdc-top-app-bar__section";
-    return `${sectionClassName} ${sectionClassName}--${value}`;
-};
+const Wrapper = styled.nav`
+    position: relative;
+    background-color: #1A64D7;
+    padding: 20px 32px;
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.25);
+`;
+
+const Link = styled.a`
+    font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, ui-sans-serif, system-ui, "Helvetica Neue", Arial, sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    letter-spacing: 0.15px;
+    text-decoration: none !important;
+    color: white !important;
+`;
 
 function NavigationBar() {
     return (
-        <nav className="mdc-top-app-bar mdc-top-app-bar--fixed-scrolled">
-            <div className="mdc-top-app-bar__row">
-                <section className={itemAlignmentClassName("align-start")}>
-                    <span className="mdc-top-app-bar__title">
-                        <a href="https://kristorres.github.io">
-                            kristorres.github.io
-                        </a>
-                    </span>
-                </section>
-            </div>
-        </nav>
+        <Wrapper>
+            <Link href="https://kristorres.github.io">
+                kristorres.github.io
+            </Link>
+        </Wrapper>
     );
 }
 
