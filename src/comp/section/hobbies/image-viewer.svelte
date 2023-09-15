9746b2effeeb96df312f40397a75f66105645f6a
Kris Torres <torres-kris@outlook.com>
2023-09-15 16:50:35 -0700

    Create the ImageViewer and YouTubeVideo components

---
+++ src/comp/section/hobbies/image-viewer.svelte
@@ -0,0 +1,94 @@
+<script>
+    export let close
+    export let images
+    export let index
+
+    import {
+        Button,
+        Flex,
+        Grid,
+        Icon,
+        Paper,
+        Screen,
+        Text,
+        Titlebar,
+    } from "@axel669/zephyr"
+
+    import Image from "$/comp/image.svelte"
+
+    const imageCount = images.length
+
+    const goToPreviousImage = () => {
+        if (index === 0) {
+            index = imageCount - 1
+            return
+        }
+
+        index -= 1
+    }
+
+    const goToNextImage = () => {
+        if (index === imageCount - 1) {
+            index = 0
+            return
+        }
+
+        index += 1
+    }
+
+    $: currentImage = images[index] ?? null
+
+    $: title = (currentImage === null)
+        ? "Image Viewer"
+        : `${currentImage.name} (${index + 1}/${imageCount})`
+</script>
+
+<Screen width="100%">
+    <Paper card square l-main="center" l-cross="center">
+        <Titlebar slot="header">
+            <Text title over-x="auto" slot="title">
+                {title}
+            </Text>
+
+            <Button compact m="4px" on:click={close} slot="action">
+                <Icon name="x" t-sz="20px" />
+            </Button>
+        </Titlebar>
+
+        {#if currentImage === null}
+            <Text>No images available.</Text>
+        {:else}
+            {#key index}
+                <Image
+                    url={currentImage.url}
+                    description={currentImage.description}
+                />
+            {/key}
+        {/if}
+
+        <Grid cols="1fr 1fr" slot="footer">
+            <Button
+                color="primary"
+                t-sz="&text-size-normal"
+                on:click={goToPreviousImage}
+                disabled={imageCount < 2}
+            >
+                <Flex direction="row" pad="0px">
+                    <Icon name="player-skip-back-filled" />
+                    PREVIOUS
+                </Flex>
+            </Button>
+            <Button
+                color="primary"
+                t-sz="&text-size-normal"
+                on:click={goToNextImage}
+                disabled={imageCount < 2}
+            >
+                <Flex direction="row" pad="0px">
+                    NEXT
+                    <Icon name="player-skip-forward-filled" />
+                </Flex>
+            </Button>
+        </Grid>
+    </Paper>
+</Screen>
---
+++ src/comp/section/hobbies/youtube-video.svelte
@@ -0,0 +1,52 @@
+<script>
+    export let id
+    export let name
+    export let width = "100%"
+
+    import {wsx} from "@axel669/zephyr"
+
+    const wind = {
+        container: {
+            block: true,
+            pos: "relative",
+            "bg-c": "&background-layer",
+            w: (width === "100%")
+                ? width
+                : `min(100%, ${width})`,
+        },
+        video: {
+            pos: "absolute",
+            x: "0px",
+            y: "0px",
+            "b-w": "0px",
+            w: "100%",
+            h: "100%",
+        },
+    }
+
+    const videoFeatures = [
+        "accelerometer",
+        "autoplay",
+        "clipboard-write",
+        "encrypted-media",
+        "gyroscope",
+        "picture-in-picture",
+        "web-share",
+    ]
+</script>
+
+<style>
+    youtube-video {
+        aspect-ratio: 16 / 9;
+    }
+</style>
+
+<youtube-video use:wsx={wind.container}>
+    <iframe
+        use:wsx={wind.video}
+        src="https://www.youtube.com/embed/{encodeURIComponent(id)}?rel=0"
+        title={name}
+        allow={videoFeatures.join("; ")}
+        allowfullscreen
+    />
+</youtube-video>
